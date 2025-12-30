2bc949231a708a54c13227b01e7e1210564786f1
NotJustAkarsh <atechnical8@gmail.com>
2025-12-30 12:45:27 +0530

    v1

---
+++ 05_css/src/App.jsx
@@ -0,0 +1,15 @@
+import Button from "./components/Button/Button"
+import Header from "./components/Header/Header"
+import Navbar from "./components/Navbar/Navbar"
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Navbar/>
+      <Button/>
+    </div>
+  )
+}
+
+export default App
---
+++ 05_css/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <h3>Akarsh</h3>
+      <button>Login</button>
+    </div>
+  )
+}
+
+export default Header
