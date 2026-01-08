3a7396dbcd17eb322dc07a7f9f8e29d465f1f3e7
NotJustAkarsh <atechnical8@gmail.com>
2026-01-09 00:59:58 +0530

    v1

---
+++ 21_context-api/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Button from './components/Button'
+
+const App = () => {
+
+  return (
+    <div>
+      <Navbar>
+        <h1>This is Navbar</h1>
+        <h3>Bohot Accha Navbar</h3>
+      </Navbar>
+      <Button/>
+    </div>
+  )
+}
+
+export default App
